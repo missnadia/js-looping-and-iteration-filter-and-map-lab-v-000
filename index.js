c8f0707b888f337b34bc320c866a7c1fedba3760
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-29 18:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here:
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {
+    return driver.name;
+  });
+}
+
+function exactMatch (drivers, attr) {
+  return drivers.filter(function (driver) {
+    return driver.attr === attr;
+  }).map(function (driver) {
+    return driver.name;
+  });
+  
+}
+
+function exactMatchToList (drivers, attr) {
+  return exactMatch(drivers, revenue).map(function (driver) {
+    return driver.name;
+  });
+}
